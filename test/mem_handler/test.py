a4e95814e2c1f460ea6c0c3d5426f8189b23af07
Marcus Alagar <mvala079@gmail.com>
2026-05-09 21:45:58 -0700

    Tested regular load cycle successfully

--- test/mem_handler/test.py
+++ test/mem_handler/test.py
@@ -7,7 +7,7 @@ from cocotb.triggers import ClockCycles, ReadWrite, ReadOnly, NextTimeStep, Risi
 
 
 @cocotb.test()
-async def test_project(dut):
+async def test_fetch(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
@@ -27,7 +27,7 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Test regular fetch cycle")
 
     # Wait for one clock cycle to see the output values
     # https://docs.cocotb.org/en/stable/timing_model.html
@@ -93,6 +93,81 @@ async def test_project(dut):
 
     await ClockCycles(dut.clk, 1)  # De-request is latched on clock cycle
     dut.fetch_req.value = 0
+
+    await ClockCycles(dut.clk, 5)
+
+
+@cocotb.test()
+async def test_load(dut):
+    dut._log.info("Start")
+
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, unit="us")
+    cocotb.start_soon(clock.start())
+
+    # Reset
+    dut._log.info("Reset")
+    dut.rst.value = 1
+    dut.fetch_req.value = 0
+    dut.fetch_addr.value = 0
+    dut.mem_req.value = 0
+    dut.mem_w_req.value = 0
+    dut.mem_addr.value = 0
+    dut.mem_w_val.value = 0
+    dut.miso.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst.value = 0
+
+    dut._log.info("Test regular load cycle")
+
+    # Wait for one clock cycle to see the output values
+    # https://docs.cocotb.org/en/stable/timing_model.html
+    # cocotb will not have triggered the downstream effects
+    # of this clock cycle
+    await ClockCycles(dut.clk, 1)
+    # By ReadWrite it will process the downstream effects,
+    # but further writes can be made and affect the circuit
+    # (i.e. like a combinational circuit)
+    # After ReadOnly, all values from this clock cycle
+    # are completed and we can see the effects
+    await ReadOnly()
+
+    assert dut.cs.value == 0b111
+    assert dut.sck.value == 0
+
+    dut._log.info("On fetch request, should not see any combinational output")
+    await NextTimeStep()
+    dut.mem_req.value = 1
+    dut.mem_addr.value = 15
+
+    await ReadWrite()
+    assert dut.cs.value == 0b111
+    assert dut.sck.value == 0
+
+    dut._log.info("But then on next cycle, should have (de)asserted CS")
+    await ClockCycles(dut.clk, 1)
+    await ReadOnly()
+
+    assert dut.cs.value == 0b101
+    assert dut.sck.value == 0
+
+    dut._log.info("Expect to see 0x03 on MOSI on rising edge of SCK")
+    await check_mosi_value(dut, 0x03, 8)
+
+    dut._log.info("Expect to see 15 as the 24-bit address through SPI on MOSI")
+    await check_mosi_value(dut, 15, 24)
+
+    dut._log.info("Write intended date through MISO")
+    data = 0xe2
+    await write_miso_value(dut, data, 8)
+
+    dut._log.info("Now wait for expected fetch_valid, then de-request")
+    await RisingEdge(dut.mem_valid)
+    await ReadOnly()
+    assert dut.mem_val.value == data 
+
+    await ClockCycles(dut.clk, 1)  # De-request is latched on clock cycle
+    dut.mem_req.value = 0
 
     await ClockCycles(dut.clk, 5)
 
